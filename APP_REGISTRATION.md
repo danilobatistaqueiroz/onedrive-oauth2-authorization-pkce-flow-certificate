f982bbb7e5d2c9e469460fbb8915cbd400ce6f5a
danilo batista queiroz <danilobatistaqueiroz@gmail.com>
2022-12-03 18:38:47 -0300

    auth download client side

--- APP_REGISTRATION.md
+++ APP_REGISTRATION.md
@@ -7,5 +7,35 @@ https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-
 # Portal Azure, para configurar Apps:  
 https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
 
-# 
+
 https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/5ca13223-4cf7-4bf3-9ba9-a8b7fe9ccdd6/isMSAApp~/true
+
+
+
+##### Gerar o Certificado e o JWT
+//é necessário gerar um certificado e aplicar no azure app registrations  
+//no azure app registration, configurar a url de retorno, o tipo de app que pode ser desktop, mobile, web, spa  
+//configurar para aceitar pkce  
+
+//gerar um encrypted private key  
+`openssl genrsa -des3 -passout pass:x -out keypair.key 2048`
+//gerar uma private key  
+`openssl rsa -passin pass:x -in keypair.key -out ./private.key`  
+//gerar o certificado  
+`openssl req -newkey rsa:4096  -x509  -sha512  -days 365 -nodes -out certificate.pem -keyout privatekey.pem`  
+//imprimir a public key  
+`openssl x509 -pubkey -noout -in certificate.pem`  
+
+##### Verificar se está usando a Private Key correta
+//Run the following command to view the modulus of the certificate.  
+`openssl x509 -noout -modulus -in server.crt | openssl md5`  
+//Now you will receive the modulus something like a77c7953ea5283056a0c9ad75b274b96  
+//Run the following command to view the modulus of the private key.  
+`openssl rsa -noout -modulus -in myserver.key | openssl md5`  
+//Now you should get the modulus as same as certificate modulus above. i.e a77c7953ea5283056a0c9ad75b274b96  
+
+##### Pegar o figerprint do certificado e gerar o claim x5t para o JWT
+`openssl x509 -fingerprint -in certificates/certificate.pem -noout`  
+//SHA1 Fingerprint=53:D7:03:93:B1:FC:09:05:98:08:47:34:58:C9:E2:85:85:B7:1F:8D  
+//remover os :
+`let x5t = safeBase64EncodedThumbprint('53D70393B1FC09059808473458C9E28585B71F8D')`  
--- server.js
+++ server.js
@@ -20,16 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/set', (req, res) => {
-  localStorage.setItem("access_token", 'EwB4A8l6BAAUkj1NuJYtTVha+Mogk+HEiPbQo04AAVN/E1w0ed6bdJvvo70r2ZlSP3zGsvXpMlJJQMxDZpvzMsVQuqcuV0X2vsVAzVFcQbBxflxyrxfqSpE7oTbHHiGgxr+J+QQ9ZdUPkRF+mlyaKe8fKmJo8W7LNFt5FpHamP2a01n2fLtWPsoRLgUDl6jiO2iGfvm0iH/S/CKjrLDXw9KlC4+gHLjEgJfofm7rEKoNxw2FczA/LXKZtF3fFHWeVsFw52lV4hoh+3pGjD+ywCERcQRpH9I4mKtzy0D91BSfLL0nvmnf2InRZyTyr3CQTHuDp+0qG442BPTRCv/XYqXYWxABwQ6X13OZkWHNjeYahE30+HoPxLhxXjE69pEDZgAACA3/3juhq7KESAK9zUBWbgKuGct0oL6MectM1J8oHsYytLK2bx5mqgcoxsh2MJllt2UihqFhQhk09ol2x+tDqGcHeZcgGjH7ZGBQQfvhIPz7Ur1BTKlumlrL8l3N1WEpv0Vp9VmAV7tRyiOkI1+rsQtDg/ECT5VxVfVqfmoFwCSggfv6H1p8JnfGM9sfCnbRxyYMr9SDYXPyO9GeuTfLsoe3Sy2lxTTWjFyjXg/9lLjNdGye2neICD3DJVKqlk72Mxq7tNmEZ+e85eG9hGBj8QftpE32QuRJF+i7ErKg3ii8KxsQY7crVc6Ci2scP6B6MZ2qCBvVEQftg91VEBCKTG/EGNfJEEvXO6feB2D+875A3zRi+lUwJP2CTu2dYiABNlS36t14pVFonpub5onHBeTn/OEj0CtyLNTUrqSP68llh7Xtv31A26qbg5c0OY/GE2+x3c0OE0tvCvrOu5T2wFdefraS7StGRyiO69eVLdPNXav6lSWLTch2z3wNzmE/F/L2U1GnEE9p2zeBDHkQImz2P56u9NHVNh+lODSiOV4SbClNAXsqaWMVZ3LRZsfKErkLZQVN6C2TEbcclHHhcnJLxW2WWlyGeOpZDLMHb9eA/naHntPq/iPjaY0a21CfGbQkCuyiktITQvcBLq97IcF+2uT7Ams9tpz+DflQdORsHwS2jHZoqGRHl0Bb0y2O2qrPN+ppN6pxpc5NPpzZuSmRgMKul17FL51oMdFp7kVUSKuCaakkih4pfxOEQOnTQbn1g/UeW2JExPeWOG3De//5rpcC');
-  res.render('main', { authCode: '', hastoken: true });
-});
-
-app.get('/logoff', (req, res) => {
-  localStorage.setItem("access_token", '');
-  res.render('main', { authCode: '', hastoken: false });
-});
-
 app.get('/', function (req, res) {
   let hastoken = localStorage.getItem("access_token") ? true : false;
   res.render('main', { authCode: '', hastoken: hastoken });
@@ -43,76 +33,5 @@ app.get('/webapp', async function (req, res) {
     res.render('main', { authCode: '' });
   }
 });
-
-app.post('/code', (req, res) => {
-  localStorage.setItem("codeVerifier", req.body.codeVerifier);
-  res.render('main', { authCode: '', hastoken: false });
-});
-
-app.get('/listdrives', (req, res) => {
-  let token = localStorage.getItem('access_token')
-  var options = {
-    hostname: 'graph.microsoft.com',
-    port: 443,
-    path: '/v1.0/me/drives',
-    method: 'GET',
-    headers: {
-      'Authorization': `bearer ${token}`
-    }
-  };
-  let data = [];
-  var req = https.request(options, (res) => {
-    res.on('data', (d) => {
-      data.push(d);
-    });
-    res.on('end', () => {
-      let b = Buffer.concat(data)
-      fs.writeFile("./tests.json", b, "binary", function (err) { });
-    });
-  });
-
-  req.on('error', (e) => {
-    console.err(e);
-  });
-
-  req.write('');
-  req.end();
-
-  res.render('main', { authCode: '', hastoken: true });
-});
-
-app.get('/listdrive', (req, res) => {
-  let token = localStorage.getItem('access_token')
-  var options = {
-    hostname: 'graph.microsoft.com',
-    port: 443,
-    path: '/v1.0/me/drives/8d708a9b80ebb4b7/root/children/1-1000.zip',
-    method: 'GET',
-    headers: {
-      'Authorization': `bearer ${token}`
-    }
-  };
-  let data = [];
-  var req = https.request(options, (res) => {
-    res.on('data', (d) => {
-      data.push(d);
-    });
-    res.on('end', () => {
-      let b = Buffer.concat(data)
-      fs.writeFile("./test.txt", b, "binary", function (err) { });
-    });
-  });
-
-  req.on('error', (e) => {
-    console.err(e);
-  });
-
-  req.write('');
-  req.end();
-
-  res.render('main', { authCode: '', hastoken: true });
-});
-
-
 
 app.listen(3000);
